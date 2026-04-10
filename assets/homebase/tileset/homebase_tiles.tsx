f591423f165a15d3075498be5119532e7f6af78a
Jairo San Miguel Sang <jairosanmiguelsang@gmail.com>
2026-04-10 18:02:56 +0200

    made an initial homebase tilemap and gathered a tileset

---
+++ assets/homebase/tileset/homebase_tiles.tsx
@@ -0,0 +1,367 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="homebase_tiles" tilewidth="56" tileheight="64" tilecount="74" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Environment/BoxBlue.png" width="26" height="26"/>
+ </tile>
+ <tile id="1">
+  <image source="Environment/BoxRed.png" width="26" height="26"/>
+ </tile>
+ <tile id="2">
+  <image source="Environment/BoxYellow.png" width="26" height="26"/>
+ </tile>
+ <tile id="3">
+  <image source="Environment/Door01.png" width="54" height="49"/>
+ </tile>
+ <tile id="4">
+  <image source="Environment/Door02.png" width="51" height="49"/>
+ </tile>
+ <tile id="5">
+  <image source="Environment/Light.png" width="22" height="12"/>
+ </tile>
+ <tile id="6">
+  <image source="Environment/TankAlien.png" width="56" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="Environment/TankSpinal.png" width="45" height="49"/>
+ </tile>
+ <tile id="8">
+  <image source="Floor/01.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <image source="Floor/02.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <image source="Floor/03.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <image source="Floor/04.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <image source="Floor/05.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <image source="Floor/06.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <image source="Floor/07.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <image source="Floor/08.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="Floor/09.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <image source="Floor/10.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <image source="Floor/11.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <image source="Floor/12.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <image source="Floor/13.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <image source="Floor/14.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <image source="Floor/15.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <image source="Floor/16.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <image source="Floor/17.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <image source="Floor/18.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <image source="Floor/19.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <image source="Floor/20.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <image source="Floor/21.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <image source="Floor/22.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <image source="Floor/23.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <image source="Floor/24.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <image source="Floor/25.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <image source="Floor/26.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <image source="Floor/27.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <image source="Floor/28.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <image source="Floor/29.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <image source="Floor/30.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <image source="Floor/31.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <image source="Floor/32.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <image source="Floor/33.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <image source="Floor/34.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <image source="Floor/35.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="43">
+  <image source="Floor/36.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <image source="Floor/37.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <image source="Floor/38.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <image source="Floor/39.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <image source="Floor/40.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <image source="Floor/41.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <image source="Floor/42.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="50">
+  <image source="Floor/43.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <image source="Floor/44.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <image source="Floor/45.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <image source="Floor/46.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <image source="Floor/47.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <image source="Objects/Beam.png" width="11" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="11" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <image source="Objects/Button.png" width="16" height="29"/>
+ </tile>
+ <tile id="57">
+  <image source="Objects/Function.png" width="7" height="16"/>
+ </tile>
+ <tile id="58">
+  <image source="Objects/Health.png" width="18" height="18"/>
+ </tile>
+ <tile id="59">
+  <image source="Wall Grey/Background.png" width="32" height="32"/>
+ </tile>
+ <tile id="60">
+  <image source="Wall Grey/LeftBottom.png" width="32" height="32"/>
+ </tile>
+ <tile id="61">
+  <image source="Wall Grey/LeftTop.png" width="32" height="32"/>
+ </tile>
+ <tile id="62">
+  <image source="Wall Grey/MiddleBottom.png" width="32" height="32"/>
+ </tile>
+ <tile id="63">
+  <image source="Wall Grey/MiddleTop.png" width="32" height="32"/>
+ </tile>
+ <tile id="64">
+  <image source="Wall Grey/MiddleTopAir.png" width="32" height="32"/>
+ </tile>
+ <tile id="65">
+  <image source="Wall Grey/RightBottom.png" width="32" height="32"/>
+ </tile>
+ <tile id="66">
+  <image source="Wall Grey/RightTop.png" width="32" height="32"/>
+ </tile>
+ <tile id="67">
+  <image source="Wall Red/LeftBottom.png" width="32" height="32"/>
+ </tile>
+ <tile id="68">
+  <image source="Wall Red/LeftTop.png" width="32" height="32"/>
+ </tile>
+ <tile id="69">
+  <image source="Wall Red/MiddleBottom.png" width="32" height="32"/>
+ </tile>
+ <tile id="70">
+  <image source="Wall Red/MiddleTop.png" width="32" height="32"/>
+ </tile>
+ <tile id="71">
+  <image source="Wall Red/MiddleTopAir.png" width="32" height="32"/>
+ </tile>
+ <tile id="72">
+  <image source="Wall Red/RightBottom.png" width="32" height="32"/>
+ </tile>
+ <tile id="73">
+  <image source="Wall Red/RightTop.png" width="32" height="32"/>
+ </tile>
+</tileset>
